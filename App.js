f5d4d6abba3fa7de9866672989e8aefc9fc1a25e
Sajeeb <sajeebahamed6@gmail.com>
2021-02-28 23:38:56 +0600

    state

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoal, setCourseGoal] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+  const addGoalHandler = () => {
+    // setCourseGoal([...courseGoal, enteredGoal])
+    setCourseGoal(currentGoals => [...currentGoals, enteredGoal])
+  }
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
           placeholder='name'
+          onChangeText={goalInputHandler}
         />
-        <Button title='Add' />
+        <Button title='Add' onPress={addGoalHandler} />
+      </View>
+      <View>
+        { courseGoal.map((goal, i) => <Text key={i}> {goal} </Text>) }
       </View>
     </View>
   );
