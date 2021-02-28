4347d74553c5ddf88c9cc7b0aff8853ee6accab2
Sajeeb <sajeebahamed6@gmail.com>
2021-02-28 22:35:31 +0600

    stylesheet

--- App.js
+++ App.js
@@ -1,50 +1,33 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        padding: 50,
-        flexDirection: 'row',
-        width: '80%',
-        height: 300,
-        justifyContent: 'space-around',
-        alignItems: 'stretch'
-      }}
-    >
-     <View
-        style={{
-          padding: 50,
-          backgroundColor: 'red',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-     >
-       <Text> 1 </Text>
-     </View>
-      <View
-        style={{
-          padding: 50,
-          backgroundColor: 'green',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text> 2 </Text>
-      </View>
-
-      <View
-        style={{
-          padding: 50,
-          backgroundColor: 'cyan',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text> 2 </Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder='name'
+        />
+        <Button title='Add' />
       </View>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+  }
+})
